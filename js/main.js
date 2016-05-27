d5445dc215a3b2740d29a43c45849171058c9c55
Lcfvs <michael.rouges@gmail.com>
2016-05-27 19:20:43 +0200

    + middleware sample

---
+++ js/main.js
@@ -0,0 +1,34 @@
+void function(global) {
+    'use strict';
+    
+    // onSubmit middleware
+    
+    var
+    cheats,
+    onSubmit;
+    
+    cheats = global['form.cheats'];
+    
+    onSubmit = function(event) {
+        var
+        form;
+        
+        form = this;
+        
+        if (form.classList.contains('cheats')) {
+            return
+        }
+        
+        event.preventDefault();
+        
+        return false;
+    };
+    
+    cheats.use(function(form, next) {
+        form.addEventListener('submit', onSubmit);
+        
+        next();
+    });
+    
+	cheats.observe(global.document);
+}(this);
